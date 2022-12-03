0388caff64d0e890cc7c9a3c74d6710307c3f969
tkburis <tkburis@gmail.com>
2022-12-03 12:07:12 +0000

    Refactored self.path

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ from .config import *
 class IsolateSandbox:
     def __init__(self, id) -> None:
         self.id = id
-        self.path = None
 
+    # Initialize sandbox and return path.
     def create(self):
         proc = subprocess.run(["isolate",
                                "--box-id", f"{self.id}",
                                "--init"], capture_output=True)
-        self.path = proc.stdout[:-1].decode("utf-8") + "/box"
+        return proc.stdout[:-1].decode("utf-8") + "/box"
 
     def cleanup(self):
         subprocess.run(["isolate",
                         "--box-id", f"{self.id}",
                         "--cleanup"])
 
-    def run_file(self, file_name : str):
-        self.create()
+    def run_file(self, file_name: str):
+        path = self.create()
         subprocess.run(['cp',
                         file_name,
-                        self.path])
+                        path])
         if file_name.endswith(".py"):
             subprocess.run(["isolate",
                             "--box-id", f"{self.id}",
@@ -31,15 +31,17 @@ class IsolateSandbox:
             pass
         self.cleanup()
 
-    def run_code(self, code :str):
-        self.create()
+    # Return `verdict` given code and test cases.
+    def run_code(self, code: str):
+        path = self.create()
         subprocess.run(['touch',
-                        self.path + "/code.py"])
+                        path + "/code.py"])
         subprocess.run(['echo',
-                        code,], stdout=open(self.path + "/code.py", "w"))
+                        code,], stdout=open(path + "/code.py", "w"))
         proc = subprocess.run(["isolate",
                         "--box-id", f"{self.id}",
                         "--run", PYTHON_PATH, "code.py"])
+        
         self.cleanup()
 
 if __name__ == '__main__':
