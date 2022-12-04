faa12e9ff0f8e1a08a411cc83ac1b4c8090db5c1
L3ungj <justin.leungj@gmail.com>
2022-12-04 00:08:51 +0000

    Update verdict.py

--- verdict.py
+++ verdict.py
@@ -13,3 +13,22 @@ class Verdict(Enum):
     
     def __str__(self) -> str:
         return self.__repr__()
+
+    def is_AC(self) -> bool:
+        return self is Verdict.AC
+
+    def long_name(self) -> str:
+        if self is Verdict.AC:
+            return 'Accepted'
+        elif self is Verdict.WA:
+            return 'Wrong Answer'
+        elif self is Verdict.TLE:
+            return 'Time Limit Exceeded'
+        elif self is Verdict.RE:
+            return 'Runtime Error'
+        elif self is Verdict.CE:
+            return 'Compilation Error'
+        elif self is Verdict.SE:
+            return 'System Error'
+        else:
+            raise Exception('Invalid Verdict')
