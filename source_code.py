e98a7e4bddf3248d9c5b23e53d15950a6e49cc5f
L3ungj <justin.leungj@gmail.com>
2023-02-24 19:41:45 +0000

    fix splitlines bug

--- source_code.py
+++ source_code.py
@@ -106,10 +106,11 @@ class SourceCode:
             except:
                 error = error_raw
             else:
+                endl = '\n'
                 error = (
                     f'{error_raw[(error_raw.rfind("Exception: ")+11):]}\n'
                     f'  Line {line_num}:\n'
-                    f'    {self.code.splitlines()[line_num-1]}'
+                    f'    {self.code.split(endl)[line_num-1]}'
                 )
         else:
             error = ''
